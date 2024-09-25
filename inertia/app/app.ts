4b166c4fb4b9ef4ce6078a476fcc3481ea9af168
MaloLebrin <malolebrin@gmail.com>
2024-09-25 06:44:25 +0200

    add unpoly

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -7,6 +7,9 @@ import type { DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 
+import 'unpoly/unpoly.css'
+import 'unpoly/unpoly.js'
+
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
 createInertiaApp({
