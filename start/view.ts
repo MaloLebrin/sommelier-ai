054ced9d42ae3ceefff337b322f4144ed90511ca
MaloLebrin <malolebrin@gmail.com>
2024-09-25 22:37:20 +0200

    refactor with new architecture

--- start/view.ts
+++ start/view.ts
@@ -1,63 +1,18 @@
-// import i18nManager from '@adonisjs/i18n/services/main'
-import vite from '@adonisjs/vite/services/main'
-import router from '@adonisjs/core/services/router'
-import { HttpContext } from '@adonisjs/core/http'
+import edge from 'edge.js'
+import env from '#start/env'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as heroIcons } from '@iconify-json/heroicons'
 
-// const dateFormatter = i18nManager.locale('fr')
+/**
+ * Add heroIcons collection
+ */
+// addCollection(heroIcons)
+/**
+ * Register a plugin
+ */
+edge.use(edgeIconify)
 
-export function space(num: number) {
-  return `calc(${num} * var(--space))`
-}
-
-export function currentYear() {
-  return new Date().getFullYear()
-}
-
-// export function formatDate(value: Date) {
-//   return dateFormatter.formatDate(value, {
-//     dateStyle: 'medium',
-//   })
-// }
-
-export function route(...args: Parameters<typeof router.makeUrl>) {
-  return router.makeUrl(...args)
-}
-
-export function csrfField() {
-  const { request } = HttpContext.getOrFail()
-
-  return Html.createElement('input', { type: 'hidden', value: request.csrfToken, name: '_csrf' })
-}
-
-function Image(props: JSX.HtmlImageTag) {
-  if (!props.src) {
-    throw new Error('Missing src prop for <Image />')
-  }
-
-  const url = vite.assetPath(props.src)
-
-  return Html.createElement('img', { src: url, alt: props.alt || '', class: props.class })
-}
-
-async function Entrypoint(props: { entrypoints: string[] }) {
-  const assets = await vite.generateEntryPointsTags(props.entrypoints)
-
-  const elements = assets.map((asset) => {
-    if (asset.tag === 'script') {
-      return Html.createElement('script', {
-        ...asset.attributes,
-      })
-    }
-
-    return Html.createElement('link', {
-      ...asset.attributes,
-    })
-  })
-
-  return Html.createElement(Html.Fragment, {}, elements)
-}
-
-export const Vite = {
-  Entrypoint,
-  Image,
-}
+/**
+ * Define a global property
+ */
+edge.global('appUrl', env.get('APP_URL'))
