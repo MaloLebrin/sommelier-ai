d64481d008c9208c800d3b0bf6cf9713cc573b0c
MaloLebrin <malolebrin@gmail.com>
2024-09-26 10:13:23 +0200

    Refactor controllers and add About page

--- app/pages/controllers/about_controller.ts
+++ app/pages/controllers/about_controller.ts
@@ -1,7 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
-export default class HomeController {
+export default class AboutController {
   async render({ view }: HttpContext) {
-    return view.render('pages/home')
+    return view.render('pages/About')
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -10,5 +10,7 @@
 import router from '@adonisjs/core/services/router'
 
 const HomeController = () => import('#pages/controllers/home_controller')
+const AboutController = () => import('#pages/controllers/about_controller')
 
 router.get('/', [HomeController, 'render']).as('pages.home')
+router.get('/about', [AboutController, 'render']).as('pages.about')
