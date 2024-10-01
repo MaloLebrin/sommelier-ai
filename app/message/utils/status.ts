11dd322add95d7843c4b4322ea52477788753ab5
MaloLebrin <malolebrin@gmail.com>
2024-10-01 14:31:58 +0200

    🧑‍💻 Add factories

--- app/message/utils/status.ts
+++ app/message/utils/status.ts
@@ -1,10 +1,21 @@
 import type { MessageStatusEnum } from '#message/types/status'
 
-export const MessageStatusMap = new Map<MessageStatusEnum, string>([
-  ['draft', 'brouillon'],
-  ['read', 'lu'],
-  ['received', 'reçu'],
-  ['sent', 'envoyé'],
+const DRAFT = 'draft'
+const READ = 'read'
+const RECEIVED = 'received'
+const SENT = 'sent'
+
+export const MessageStatusMap = new Map<
+  MessageStatusEnum,
+  {
+    key: MessageStatusEnum
+    translation: string
+  }
+>([
+  [DRAFT, { key: DRAFT, translation: 'draft' }],
+  [READ, { key: READ, translation: 'read' }],
+  [RECEIVED, { key: RECEIVED, translation: 'received' }],
+  [SENT, { key: SENT, translation: 'sent' }],
 ])
 
 export const messageStatus = [...MessageStatusMap.keys()]
--- database/factories/message_factory.ts
+++ database/factories/message_factory.ts
@@ -1,8 +1,13 @@
 import Message from '#message/models/message'
+import { MessageStatusMap } from '#message/utils/status'
 import factory from '@adonisjs/lucid/factories'
 
 export const MessageFactory = factory
-  .define(Message, async () => {
-    return {}
+  .define(Message, ({ faker }) => {
+    return {
+      content: faker.lorem.words(44),
+      readAt: null,
+      status: MessageStatusMap.get('sent')?.key,
+    }
   })
   .build()
