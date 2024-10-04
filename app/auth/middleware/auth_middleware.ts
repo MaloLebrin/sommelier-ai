aec201709fc34b4611e62a54743e509f95219156
MaloLebrin <malolebrin@gmail.com>
2024-10-04 11:42:23 +0200

    :wrench: config auth session

--- app/auth/middleware/auth_middleware.ts
+++ app/auth/middleware/auth_middleware.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
-// import type { Authenticators } from '@adonisjs/auth/types'
+import type { Authenticators } from '@adonisjs/auth/types'
 
 /**
  * Auth middleware is used authenticate HTTP requests and deny
@@ -13,13 +13,13 @@ export default class AuthMiddleware {
   redirectTo = '/login'
 
   async handle(
-    _ctx: HttpContext,
-    next: NextFn
-    // options: {
-    //   guards?: (keyof Authenticators)[]
-    // } = {}
+    ctx: HttpContext,
+    next: NextFn,
+    options: {
+      guards?: (keyof Authenticators)[]
+    } = {}
   ) {
-    // await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
+    await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
     return next()
   }
 }
--- app/auth/middleware/guest_middleware.ts
+++ app/auth/middleware/guest_middleware.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
-// import type { Authenticators } from '@adonisjs/auth/types'
+import type { Authenticators } from '@adonisjs/auth/types'
 
 /**
  * Guest middleware is used to deny access to routes that should
@@ -16,15 +16,15 @@ export default class GuestMiddleware {
   redirectTo = '/'
 
   async handle(
-    _ctx: HttpContext,
-    next: NextFn
-    // options: { guards?: (keyof Authenticators)[] } = {}
+    ctx: HttpContext,
+    next: NextFn,
+    options: { guards?: (keyof Authenticators)[] } = {}
   ) {
-    // for (let guard of options.guards || [ctx.auth.defaultGuard]) {
-    //   if (await ctx.auth.use(guard).check()) {
-    //     return ctx.response.redirect(this.redirectTo, true)
-    //   }
-    // }
+    for (let guard of options.guards || [ctx.auth.defaultGuard]) {
+      if (await ctx.auth.use(guard).check()) {
+        return ctx.response.redirect(this.redirectTo, true)
+      }
+    }
 
     return next()
   }
